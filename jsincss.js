605f06d6dd435c95fddba04f8acc3bea388d57ac
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-06-21 14:17:10 +0300

    Add files via upload

---
+++ jsincss.js
@@ -0,0 +1,18 @@
+var x = document.body.querySelectorAll("*");
+var i;
+for (i = 0; i < x.length; i++) {
+    
+            js = window.getComputedStyle(x[i]).content;
+            js = js.slice(1,-1)
+            if(js !== 'orma'){
+                eval(js);
+            }
+
+
+    x[i].addEventListener("mousedown", function(){
+        js = window.getComputedStyle(this).content;
+        js = js.slice(1,-1)
+        eval(js);
+    });
+
+}
