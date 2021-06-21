7ac140efd4b80fd2c382e2e6e5a112e785e65bd7
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-06-21 14:27:27 +0300

    Update jsincss.js

--- jsincss.js
+++ jsincss.js
@@ -1,18 +1,20 @@
-var x = document.body.querySelectorAll("*");
-var i;
-for (i = 0; i < x.length; i++) {
-    
-            js = window.getComputedStyle(x[i]).content;
+window.addEventListener('DOMContentLoaded', (event) => {
+    var x = document.body.querySelectorAll("*");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        
+                js = window.getComputedStyle(x[i]).content;
+                js = js.slice(1,-1)
+                if(js !== 'orma'){
+                    eval(js);
+                }
+
+
+        x[i].addEventListener("mousedown", function(){
+            js = window.getComputedStyle(this).content;
             js = js.slice(1,-1)
-            if(js !== 'orma'){
-                eval(js);
-            }
+            eval(js);
+        });
 
-
-    x[i].addEventListener("mousedown", function(){
-        js = window.getComputedStyle(this).content;
-        js = js.slice(1,-1)
-        eval(js);
-    });
-
-}
+    }
+});
